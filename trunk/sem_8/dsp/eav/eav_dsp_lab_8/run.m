d3df2b1140e4d716b56e9a49d00049398776757a
eav1986 <eav1986@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-05-27 14:57:10 +0000

    исправлен п.1. лр 8 цос согласно замечанию симончика: неверно вычисляется сигнал: синус частоты f: sin(f*t) - неверная формула. Правильно: sin(2*pi*f*t)

--- trunk/sem_8/dsp/eav/eav_dsp_lab_8/run.m
+++ trunk/sem_8/dsp/eav/eav_dsp_lab_8/run.m
@@ -16,7 +16,7 @@ f1 = zeros(k);
 y1 = zeros(k);
 for m = 1:k
     f1(m) = a1 * t(m) * t(m) + b1 * t(m) + A1;
-    y1(m) = sin(f1(m) * t(m));
+    y1(m) = sin(2*pi*f1(m) * t(m));
 end
 
 figure(1); clf;
@@ -38,7 +38,7 @@ f2 = zeros(k);
 y2 = zeros(k);
 for m = 1:k
     f2(m) = a2 * exp(b2*t(m)) + c2*t(m) + d2;
-    y2(m) = sin(f2(m) * t(m));
+    y2(m) = sin(2*pi*f2(m) * t(m));
 end
 
 figure(2); clf;
@@ -61,7 +61,7 @@ f3 = zeros(k);
 y3 = zeros(k);
 for m = 1:k
     f3(m) = a3 * exp(b3*t(m)) + c3*t(m)*t(m) + d3*t(m) + e3;
-    y3(m) = sin(f3(m) * t(m));
+    y3(m) = sin(2*pi*f3(m) * t(m));
 end
 
 figure(3); clf;
