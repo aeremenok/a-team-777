b2f90242f01bfc62a90e94ee1614e942029dcb93
eav1986 <eav1986@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-09-25 09:22:42 +0000

    первый коммит оот

---
+++ sem_9/oot/eav_lab1/src/eav_oot_lab_1/TextInOval.cpp
@@ -0,0 +1,26 @@
+// Copyright (C) 1991 - 1999 Rational Software Corporation
+
+#include "TextInOval.h"
+
+
+
+//##ModelId=46F677F2002E
+TextInOval::~TextInOval()
+{
+	// ToDo: Add your specialized code here and/or call the base class
+}
+
+//##ModelId=46F677F30128
+TextInOval::TextInOval()
+{
+	// ToDo: Add your specialized code here and/or call the base class
+}
+
+//##ModelId=46F6787D009C
+void TextInOval::speak() const
+{
+	// ToDo: Add your specialized code here
+	
+	static_cast<void>(0);
+}
+
