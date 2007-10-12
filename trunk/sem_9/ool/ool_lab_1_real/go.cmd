edbfea8205d7418b13a5dcac67a2b0064f5b4570
stas.sviridenko <stas.sviridenko@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-10-12 12:48:58 +0000

    настоящая лаба

---
+++ trunk/sem_9/ool/ool_lab_1_real/go.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+cd bin
+java ru.spb.etu._3305.ool.DateTest %*
+cd ..
+
+@echo on
---
+++ trunk/sem_9/ool/ool_lab_1_real/src/ru/spb/etu/_3305/ool/DateTest.java
@@ -0,0 +1,53 @@
+package ru.spb.etu._3305.ool;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+
+/**
+ * Выводит данные о разрабочиках, дату получения задания и дату сдачи задания
+ */
+public class DateTest
+{
+    public static void main( String args[] )
+    {
+        BufferedWriter writer = null;
+        try
+        {
+            writer = new BufferedWriter( new OutputStreamWriter( System.out, "cp866" ) );
+            writer.write( "\nРазработчики: Свириденко С.В., Лысенко А.А., Бессонов А.В.\n" );
+            writer.write( "Дата получения задания: 11.10.2007\t11:46\n" );
+
+            Date date = new Date();
+
+            writer.write(
+                    "Дата сдачи задания:     " + date.getDate() + "." + ( date.getMonth() + 1 ) + "." +
+                    ( date.getYear() + 1900 ) + "\t" + date.getHours() + ":" + date.getMinutes() + "\n" );
+            writer.flush();
+        }
+        catch ( UnsupportedEncodingException e )
+        {
+            e.printStackTrace();
+        }
+        catch ( IOException e )
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if ( writer != null )
+            {
+                try
+                {
+                    writer.close();
+                }
+                catch ( IOException e )
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
