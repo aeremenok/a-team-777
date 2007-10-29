56d882e5511f8878c0ed1a70fe0ffab2761fbac9
stas.sviridenko <stas.sviridenko@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-10-29 13:45:57 +0000

    4-я лаба по оояис с разбегающимися кругами

---
+++ sem_9/ool/ool_lab_4/src/TestApplet.java
@@ -0,0 +1,150 @@
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by IntelliJ IDEA. User: ssv Date: 29.10.2007 Time: 12:19:01 To change this template use File | Settings |
+ * File Templates.
+ */
+public class TestApplet
+        extends Applet
+{
+    /**
+     * датчик случайных чисел
+     */
+    private Random _random = null;
+
+    /**
+     * текущие размеры апплета (для заглушки onresize)
+     */
+    private Dimension _dimension = null;
+
+    /**
+     * радиус круга
+     */
+    private int _diameter = 40;
+
+    /**
+     * список кругов для отрисовки
+     */
+    private ArrayList<Circle> _circleList = null;
+
+    public void start()
+    {
+        _dimension = getSize();
+
+        addMouseMotionListener( new MouseMotionListener()
+        {
+            public void mouseDragged( MouseEvent e )
+            {
+                // ничего не делаем
+            }
+
+            public void mouseMoved( MouseEvent e )
+            {
+                // новое положение курсора
+                Point cursor = new Point( e.getX(), e.getY() );
+
+                for ( Circle c : _circleList )
+                {
+                    if ( c.contains( cursor ) ) // надо прыгать
+                    {
+                        // вычисляем в какую четверть будем прыгать
+                        int leftX = ( _dimension.width - cursor.x > cursor.x ) ? cursor.x : 0;
+                        int rightX = ( _dimension.width - cursor.x > cursor.x ) ? _dimension.width : cursor.x;
+                        int upY = ( _dimension.height - cursor.y > cursor.y ) ? cursor.y : 0;
+                        int downY = ( _dimension.height - cursor.y > cursor.y ) ? _dimension.height : cursor.y;
+
+                        // точка для прыжка
+                        int jumpX = _random.nextInt( rightX - leftX - c.diameter ) + leftX;
+                        int jumpY = _random.nextInt( downY - upY - c.diameter ) + upY;
+
+                        // двигаем круг
+                        c.moveTo( jumpX, jumpY );
+
+                        // перерисовка
+                        paint( getGraphics() );
+                    }
+                }
+            }
+        }
+        );
+
+        // fixme добавить обработчик resize
+    }
+
+    public void stop()
+    {
+        removeMouseMotionListener( getMouseMotionListeners()[0] );
+    }
+
+    public void init()
+    {
+        // инициализируем новый датчик
+        _random = new Random( System.currentTimeMillis() );
+
+        // пересчитываем количество кругов
+        rebuild( getSize().width, getSize().height );
+    }
+
+    /**
+     * пересчёт под новый размер экрана
+     *
+     * @param width  новая ширина апплета
+     * @param height новая высота апплета
+     */
+    private void rebuild( int width, int height )
+    {
+        // создаём новый список кругов
+        _circleList = new ArrayList<Circle>();
+
+        // сколько влезет кругов максимально
+        int h = height / _diameter;
+        int w = width / _diameter;
+
+        // столько кругов будем рисовать
+        int num = h * w / 4;
+
+        // заполняем массив кругами
+        for ( int i = 0; i < num; i++ )
+        {
+            // генерируем расположение для нового круга
+            int wi = _random.nextInt( width - 2 * _diameter );
+            int he = _random.nextInt( height - 2 * _diameter );
+
+            // генерируем цвет для нового круга
+            int red = _random.nextInt( 255 );
+            int green = _random.nextInt( 255 );
+            int blue = _random.nextInt( 255 );
+            Color color = new Color( red, green, blue );
+
+            // сохраняем в массив
+            _circleList.add( new Circle( wi, he, _diameter, color ) );
+        }
+    }
+
+    public void paint( Graphics g )
+    {
+        // размеры
+        Dimension dim = getSize();
+
+        // fixme заглушка неработающего onresize
+        if ( !dim.equals( _dimension ) )
+        {
+            rebuild( dim.width, dim.height );
+            _dimension = dim;
+        }
+
+        // очистка
+        g.clearRect( 0, 0, dim.width - 1, dim.height - 1 );
+
+        // рисуем круги
+        for ( Circle c : _circleList )
+        {
+            c.draw( g );
+        }
+    }
+}
