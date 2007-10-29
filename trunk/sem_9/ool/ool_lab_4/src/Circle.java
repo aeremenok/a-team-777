a50287e6b0b435743b3b900ec1a984c889daaf50
stas.sviridenko <stas.sviridenko@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-10-29 13:45:57 +0000

    4-я лаба по оояис с разбегающимися кругами

---
+++ trunk/sem_9/ool/ool_lab_4/src/Circle.java
@@ -0,0 +1,75 @@
+import java.awt.*;
+
+/**
+ * Цветной круг
+ */
+public class Circle
+{
+    public int x;
+    public int y;
+    public int diameter;
+    public Color color;
+
+    Circle( int x, int y, int diameter, Color color )
+    {
+        this.x = x;
+        this.y = y;
+        this.diameter = diameter;
+        this.color = color;
+    }
+
+    /**
+     * рисует текущий круг
+     *
+     * @param g контекст
+     */
+    public void draw( Graphics g )
+    {
+        Color c = g.getColor();
+        g.setColor( color );
+        g.fillOval( x, y, diameter, diameter );
+        g.setColor( c );
+    }
+
+    /**
+     * проверяет точку на принадлежность кругу
+     *
+     * @param p точка для проверки
+     *
+     * @return true, если точка принадлежит кругу, false иначе
+     */
+    public boolean contains( Point p )
+    {
+        // центр окружности
+        int xCenter = x + diameter / 2;
+        int yCenter = y + diameter / 2;
+
+        // разница по координатам
+        int xDiff = p.x - xCenter;
+        int yDiff = p.y - yCenter;
+
+        // растояние от точки до точки
+        int distance = (int) Math.sqrt( ( xDiff * xDiff ) + ( yDiff * yDiff ) );
+
+        if ( distance <= diameter / 2 )
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * перемещает в точку с заданными координатами
+     *
+     * @param x абсцисса новой точки
+     * @param y ордината новой точки
+     */
+    public void moveTo( int x, int y )
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
