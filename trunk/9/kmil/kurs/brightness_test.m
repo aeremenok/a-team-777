6df010cb73f5df57d7d8b1e91a109a5077fcafd9
stas.sviridenko <stas.sviridenko@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-11-14 09:03:05 +0000

    упорядочивание структуры каталогов

---
+++ trunk/9/kmil/kurs/brightness_test.m
@@ -0,0 +1,109 @@
+% влияние освещения на гистограмму
+
+% очистка экрана
+clear all; clc;
+
+% префикс пути к картинке
+PATH_PREFIX = 'D:\PROJECTS\a-team-777\9\kmil\kurs\brightness_test\img\';
+
+% первая
+PIC1_PATH = [PATH_PREFIX, '1.jpg']; 
+
+% вторая
+PIC2_PATH = [PATH_PREFIX, '2.jpg']; 
+
+% третья
+PIC3_PATH = [PATH_PREFIX, '3.jpg']; 
+
+% количество уровней гистограммы
+LEVELS = 256;
+
+% размер блока гистограммы
+hist_block_size = uint8( 256 / LEVELS );    
+
+% 1-ая картинка
+
+    image1 = imread( PIC1_PATH );   % чтение изображения из файла
+
+    figure( 1 );
+    subplot( 3, 3, 1 );
+    imshow( image1 );   % показываем картинку
+
+    full_spectrum1 = uint8( zeros( 1, 256 ) );
+    for i = 1 : size( image1, 1 )
+        for j = 1: size( image1, 2 )
+            color = image1( i, j );
+            full_spectrum1( color + 1 ) = full_spectrum1( color + 1 ) + 1;
+        end;
+    end;    % расчёт полной гистограммы
+
+    figure( 1 );
+    subplot( 3, 3, 2 );
+    plot( full_spectrum1 ); % рисуем полную гистограмму
+
+    short_spectrum1 = uint8( zeros( 1, LEVELS ) );
+    for i = 1 : LEVELS
+        short_spectrum1( i ) = mean( full_spectrum1( 1, ( i - 1 ) * hist_block_size + 1 : i * hist_block_size + 1 ) );
+    end;    % расчёт укороченной гистограммы
+
+    figure( 1 );
+    subplot( 3, 3, 3 );
+    bar( short_spectrum1 ); % рисуем укороченную гистограмму
+
+% 2-я картинка
+
+    image2 = imread( PIC2_PATH);    % чтение изображения из файла
+
+    figure( 1 );
+    subplot( 3, 3, 4 );
+    imshow( image2 );   % показываем картинку
+
+    full_spectrum2 = uint8( zeros( 1, 256 ) );
+    for i = 1 : size( image2, 1 )
+        for j = 1: size( image2, 2 )
+            color = image2( i, j );
+            full_spectrum2( color + 1 ) = full_spectrum2( color + 1 ) + 1;
+        end;
+    end;    % расчёт полной гистограммы
+    
+    figure( 1 );
+    subplot( 3, 3, 5 );
+    plot( full_spectrum2 ); % рисуем полную гистограмму
+
+    short_spectrum2 = uint8( zeros( 1, LEVELS ) );
+    for i = 1 : LEVELS
+        short_spectrum2( i ) = mean( full_spectrum2( 1, ( i - 1 ) * hist_block_size + 1 : i * hist_block_size + 1 ) );
+    end;    % расчёт укороченной гистограммы
+
+    figure( 1 );
+    subplot( 3, 3, 6 );
+    bar( short_spectrum2 ); % рисуем укороченную гистограмму
+
+% 3-я картинка
+
+    image3 = imread( PIC3_PATH);    % чтение изображения из файла
+
+    figure( 1 );
+    subplot( 3, 3, 7 );
+    imshow( image3 );   % показываем картинку
+
+    full_spectrum3 = uint8( zeros( 1, 256 ) );
+    for i = 1 : size( image3, 1 )
+        for j = 1: size( image3, 2 )
+            color = image3( i, j );
+            full_spectrum3( color + 1 ) = full_spectrum3( color + 1 ) + 1;
+        end;
+    end;    % расчёт полной гистограммы
+    
+    figure( 1 );
+    subplot( 3, 3, 8 );
+    plot( full_spectrum3 ); % рисуем полную гистограмму
+
+    short_spectrum3 = uint8( zeros( 1, LEVELS ) );
+    for i = 1 : LEVELS
+        short_spectrum3( i ) = mean( full_spectrum3( 1, ( i - 1 ) * hist_block_size + 1 : i * hist_block_size + 1 ) );
+    end;    % расчёт укороченной гистограммы
+
+    figure( 1 );
+    subplot( 3, 3, 9 );
+    bar( short_spectrum3 ); % рисуем укороченную гистограмму
