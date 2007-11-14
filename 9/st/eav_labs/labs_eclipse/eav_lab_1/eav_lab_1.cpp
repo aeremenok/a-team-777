d77b64d1734a08d10311434ea59a15854abddcbf
eav1986 <eav1986@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-11-14 19:12:57 +0000

    набросок ко второй лабе по ст

--- 9/st/eav_labs/labs_eclipse/eav_lab_1/eav_lab_1.cpp
+++ 9/st/eav_labs/labs_eclipse/eav_lab_1/eav_lab_1.cpp
@@ -1,15 +1,29 @@
 //============================================================================
-// Name        : eav_lab_1.cpp
+// Name        : eav_lab_2.cpp
 // Author      : eav 3351
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C, Ansi-style
 //============================================================================
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+#define ICMP_ECHO ((USHORT)8) // код сообщения icmp-echo
+#define ICMP_MAXBUF ((DWORD)0x1000) // максимальный размер пакета icmp
+#define ICMP_DATA ((DWORD)32) // размер данных icmp
+#define ICMP_TIMEOUT ((DWORD)1000) // интервал
+// макрос инициализации структур
+#define constructor() { ZeroMemory(this, sizeof(*this)) }
+// структура заголовка пакета
+typedef struct tagIP_header
+{
+	tagIP_header() constructor();
+	unsigned char h_len: 4; // длина загловка
+	unsigned char version: 4; // версия протокола
+}
+
+int main(void)
+{
 	puts("Hello World!!!");
 	return EXIT_SUCCESS;
 }
