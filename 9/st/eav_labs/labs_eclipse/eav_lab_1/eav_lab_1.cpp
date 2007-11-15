347db0f6c0dd3b2d7d3b0d38fbba373db4812f3b
eav1986 <eav1986@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-11-15 17:52:15 +0000

    набран код из методы

--- 9/st/eav_labs/labs_eclipse/eav_lab_1/eav_lab_1.cpp
+++ 9/st/eav_labs/labs_eclipse/eav_lab_1/eav_lab_1.cpp
@@ -45,9 +45,9 @@ typedef struct tagICMP_MSG
         USHORT i_chksum; // контрольная сумма
         USHORT i_id; // идентификатор приложения
         USHORT i_seq; // номер в последовательности
-// атрибут в области доп.данных
+        // атрибут в области доп.данных
         DWORD timestamp; // временная метка
-} ICPM_MSG, *PICMP_MSG;
+} ICMP_MSG, *PICMP_MSG;
 
 USHORT g_appid; // xxx а это что?
 
@@ -59,14 +59,129 @@ bool _sc(int wsacode, const char* msg = 0)
     bool res = false;
     if (wsacode == SOCKET_ERROR)
     {
-        printf("wsaerror=%d,%s\n ", WSAGetLastError(), (msg)?(msg):(""));
+        printf("wsaerror=%d,%s\n ", WSAGetLastError(), (msg) ? (msg) : (""));
         res = true;
     }
     return res;
 }
 
-int main(void)
+USHORT checksum(USORT* buffer, int size)
 {
-    puts("Hello World!!!");
-    return EXIT_SUCCESS;
+    unsigned long chksum = 0;
+
+    while (size > 1)
+    {
+        chksum += *buffer++;
+        size -= sizeof(USHORT);
+    }
+
+    if (size)
+    {
+        chksum += *(UCHAR*)buffer;
+    }
+
+    chksum = (chksum >> 16) + (chksum&0xffff); // xxx что это?
+    chksum += (chksum >> 16);
+
+    return (USHORT)(~chksum); // xxx %)
+}
+
+/**
+ * разбор полученных пакетов
+ */
+bool icmp_parse(IP_HEADER *iph, ICMP_MSG *icmph, sockaddr_in* from, int len)
+{
+    if (icmph->i_id != g_appid)
+    {
+        printf("(:\n");
+        return false;
+    }
+
+    printf("Reply from %s bytes = %d TTL = %d time = %d ms",
+            inet_ntoa(from->sin_addr), len, iph->ttl, GetTickCount()
+                    -icmph->timestamp);
+
+    return true;
+}
+
+/**
+ * цикл посылки/принятия сообщений
+ */
+void echo(const char* hostname)
+{
+    hostent* he;
+    SOCKET sock;
+    WSADATA wsad;
+
+    sockaddr_in dest;
+    unsigned size;
+    char buf[ICMP_MAXBUF];
+    PIP_HEADER iph = (PIP_HEADER)buf;
+
+    // инициализация winsock
+    if (_sc(WSAStartup(MAKEWORD(2,1), &wsad)))
+    {
+        ExitProcess(-1);
+    }
+    // создание сокета icmp
+    sock = WSASocket(AF_INET, SOCK_RAW, IPPROTO_ICMP,0,0,0);
+
+    printf("Pinging host \\\\%s\n", hostname);
+
+    // получение адреса хоста по его имени
+    if ((he = gethostbyname(hostname))==0)
+    {
+        _sc(SOCKET_ERROR);
+        ExitProcess(-1);
+    }
+
+    ZeroMemory(&dest, sizeof(dest));
+    CopyMemory(&(dest.sin_addr), he->h_addr, he->h_length);
+    dest.sin_family = he->h_addrtype;
+    printf("Host IP %s\n", inet_ntoa(dest.sin_addr));
+
+    for (;;)
+    {
+        ICMP_MSG data_out;
+        int len = sizeof(dest);
+        size = sizeof(ICMP_MSG)+ICMP_DATA; // общий размер пакета
+
+        // заполнение поля необязательных данных пакета
+        memset((dhar*)(buf+sizeof(ICMP_MSG)), '#', ICMP_DATA);
+        // заполнение заголовка пакета
+        data_out.timestamp = GetTickCount(); // время отправления
+        data_out.i_type = ICMP_ECHO; // тип  
+        data_out.i_chksum = checksum((USHORT*)&data_out, size); // контольная сумма
+        
+        // отправка пакета с запросом ICMP_ECHO
+        _sc(sendto(sock, (char*)&data_out, size, 0, (sockaddr*)&dest, &len));
+        
+        // получение ответа и разбор пакета
+        do 
+            _sc(recvfrom(sock, buf, ICMP_MAXBUF, 0, (sockaddr*)&dest, &len));
+        while (
+                !icmp_parse(
+                (IP_HEADER*)buf,
+                (ICMP_MSG*)(buf + iph->h_len*4),
+                &dest,
+                ICMP_DATA)
+               );
+        
+        // пауза между посылаемыми пакетами
+        Sleep(ICMP_TIMEOUT);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ping.exe [hostname]\n");
+    }
+    else
+    {
+        g_appid = (USHORT)GetCurrentProcessId();
+        echo(argv[1]);
+    }
+    return 0;
 }
