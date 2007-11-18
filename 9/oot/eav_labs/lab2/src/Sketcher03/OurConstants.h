bd049aacc25226784490ddac3043b081442b674a
eav1986 <eav1986@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-11-18 15:40:02 +0000

    добавлен проект для контекстного меню и контейнера

---
+++ 9/oot/eav_labs/lab2/src/Sketcher03/OurConstants.h
@@ -0,0 +1,18 @@
+//Definitions of constants
+
+#ifndef OurConstants_h
+#define OurConstants_h
+
+   // Element type definitions
+   // Each type value must be unique
+   const WORD LINE = 101U;
+   const WORD RECTANGLE = 102U;
+   ///////////////////////////////////
+
+   // Color values for drawing
+   const COLORREF BLACK = RGB(0,0,0);
+   const COLORREF RED = RGB(255,0,0);
+   const COLORREF SELECT_COLOR = RGB(255,0,180);
+   ///////////////////////////////////
+
+#endif
