db15d61c11491c63d17d5cc1d06e3ba60261ebef
stas.sviridenko <stas.sviridenko@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-11-18 16:11:52 +0000

    программа для осветления изображений

---
+++ trunk/9/kmil/kurs/light.m
@@ -0,0 +1,34 @@
+% осветление базы
+
+% очистка
+clear all; clc;
+
+% параметры
+DB_PATH = 'D:\LETI\9\kmil\bases\orl_bmp\s'; % путь к базе
+NEW_DB_PATH = 'D:\LETI\9\kmil\bases\orl_bmp_test\s'; % путь к новой базе
+K = 40;          % количество классов изображений
+Q = 10;          % общее количество изображений в классе
+COLOR_CHANGE = 50;
+
+% построение баз
+for class = 1 : K
+    for file = 1 : Q
+        image = imread( [DB_PATH, num2str( class ), '\', num2str( file ), '.bmp'] );
+
+        if rem( file, 2 ) == 0 % осветляем только чётные изображения
+            for i = 1 : size( image, 1 )
+                for j = 1: size( image, 2 )
+                    color = image( i, j ) + COLOR_CHANGE;
+                    if color < 0
+                        color = 0;
+                    elseif color > 255
+                        color = 255;
+                    end;
+                    image( i, j ) = color;
+                end;
+            end;    % расчёт полной гистограммы, коррекция цвета
+        end;
+
+        imwrite( image, [NEW_DB_PATH, num2str( class ), '\', num2str( file ), '.bmp'] ); % запись полученной картинки в файл
+    end;
+end;
