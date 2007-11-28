317ea26ef3736fd9984ad3468eefb790355557ae
stas.sviridenko <stas.sviridenko@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-11-28 20:23:45 +0000

    ООЯИС-5 реформат кода

--- trunk/9/ool/ool_lab_5/src/BinarySort.java
+++ trunk/9/ool/ool_lab_5/src/BinarySort.java
@@ -4,30 +4,33 @@
 
 /**
  * @author lysenko
- *
  */
-public class BinarySort extends SortMethod {
+public class BinarySort
+    extends SortMethod
+{
+    /**
+     * @param array - сортируемый массив
+     */
+    public BinarySort(
+        short[] array )
+    {
+        super( array );
+    }
 
-	/**
-	 * @param array - сортируемый массив
-	 */
-	public BinarySort( short[] array ) {
-		super(array);
-	}
+    /**
+     * ¬ыполнить сотрировку массива на основе бинарного дерева
+     */
+    protected void sort()
+        throws InterruptedException
+    {
+        // TODO Auto-generated method stub
+    }
 
-	/**
-	 * ¬ыполнить сотрировку массива на основе бинарного дерева
-	 */
-	protected void sort() throws InterruptedException {
-		// TODO Auto-generated method stub
-
-	}
-	
-	/**
-	 * @return »м€ метода
-	 */
-	protected String getMethodName(){
-		return "Binary tree sort";
-	}
-
+    /**
+     * @return »м€ метода
+     */
+    protected String getMethodName()
+    {
+        return "Binary tree sort";
+    }
 }
--- trunk/9/ool/ool_lab_5/src/HoareSort.java
+++ trunk/9/ool/ool_lab_5/src/HoareSort.java
@@ -1,85 +1,93 @@
 /**
  * @author lysenko
- *
  */
-public class HoareSort extends SortMethod {
+public class HoareSort
+    extends SortMethod
+{
+    /**
+     * @param array - сортируемый массив
+     */
+    public HoareSort(
+        short[] array )
+    {
+        super( array );
+    }
 
-	/**
-	 * @param array - сортируемый массив
-	 */
-	public HoareSort( short[] array ) {
-		super(array);
-	}
+    /**
+     * Выполнить сотрировку массива методом Хоара Быстрая сортировка использует
+     * стратегию «разделяй и властвуй». Шаги алгоритма таковы: 1) Выбираем в
+     * массиве некоторый элемент, который будем называть опорным элементом. 2)
+     * Операция разделения массива: реорганизуем массив таким образом, чтобы все
+     * элементы, меньшие или равные опорному элементу, оказались слева от него,
+     * а все элементы, большие опорного — справа от него. 3) Рекурсивно
+     * упорядочиваем подсписки, лежащие слева и справа от опорного элемента.
+     * Базой рекурсии являются списки, состоящие из одного или двух элементов,
+     * которые уже упорядочены. Алгоритм всегда завершается, поскольку за каждую
+     * итерацию он ставит по крайней мере один элемент на его окончательное
+     * место.
+     */
+    protected void sort()
+        throws InterruptedException
+    {
+        quicksort( 0, _array.length );
+    }
 
-	/**
-	 * Выполнить сотрировку массива методом Хоара
-	 * 
-	 * Быстрая сортировка использует стратегию «разделяй и властвуй».
-	 * Шаги алгоритма таковы:
-	 * 1) Выбираем в массиве некоторый элемент, который будем называть опорным
-	 * элементом.
-	 * 2) Операция разделения массива: реорганизуем массив таким образом, чтобы
-	 * все элементы, меньшие или равные опорному элементу, оказались слева от
-	 * него, а все элементы, большие опорного — справа от него.
-	 * 3) Рекурсивно упорядочиваем подсписки, лежащие слева и справа от
-	 * опорного элемента.
-	 * Базой рекурсии являются списки, состоящие из одного или двух элементов,
-	 * которые уже упорядочены. Алгоритм всегда завершается, поскольку за
-	 * каждую итерацию он ставит по крайней мере один элемент на его
-	 * окончательное место.
-	 */
-	protected void sort() throws InterruptedException {
-		quicksort(0, _array.length);
-	}
-	
-	/**
-	 * Рекурсивная функция сортировки
-	 * @param p
-	 * @param r
-	 */
-	private void quicksort( int p, int r ) {
-		
-		int q;
-	    if (p < r)
-	    {
-	      q = partition (p, r);
-	      quicksort (p, q-1);
-	      quicksort (q+1, r);
-	    }	    
-	}
-	
-	/**
-	 * Операция разделения массива
-	 * @param p
-	 * @param r
-	 * @return
-	 */
-	private int partition ( int p, int r )
-	  {
-	    short x = _array[r];   
-	    int i = p - 1;
-	    short tmp;
-	    for (int j = p; j < r; j++)
-	      if (_array[j] <= x)
-	      {
-	        i++;
-	        tmp = _array[i];  
-	        _array[i] = _array[j];
-	        _array[j] = tmp;
-	      }
-	    
-	    tmp = _array[r];
-	    _array[r] = _array[i+1];
-	    _array[i+1] = tmp;
-	    
-	    return i + 1;
-	  }
-	
-	/**
-	 * @return Имя метода
-	 */
-	protected String getMethodName(){
-		return "Hoare Sort (quicksort)";
-	}
+    /**
+     * Рекурсивная функция сортировки
+     * 
+     * @param p
+     * @param r
+     */
+    private void quicksort(
+        int p,
+        int r )
+    {
+        int q;
+        if ( p < r )
+        {
+            q = partition( p, r );
+            quicksort( p, q - 1 );
+            quicksort( q + 1, r );
+        }
+    }
 
+    /**
+     * Операция разделения массива
+     * 
+     * @param p
+     * @param r
+     * @return
+     */
+    private int partition(
+        int p,
+        int r )
+    {
+        short x = _array[r];
+        int i = p - 1;
+        short tmp;
+        for ( int j = p; j < r; j++ )
+        {
+            if ( _array[j] <= x )
+            {
+                i++;
+                tmp = _array[i];
+                _array[i] = _array[j];
+                _array[j] = tmp;
+            }
+        }
+
+        tmp = _array[r];
+        _array[r] = _array[i + 1];
+        _array[i + 1] = tmp;
+
+        return i + 1;
+    }
+
+    /**
+     * @return Имя метода
+     */
+    protected String getMethodName()
+    {
+        return "Hoare Sort (quicksort)";
+    }
 }
