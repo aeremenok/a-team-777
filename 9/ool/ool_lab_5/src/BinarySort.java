0ecd34107258b61050ac42890977ad473b243795
scorpion1105 <scorpion1105@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-11-28 23:04:24 +0000

    ООЯИС-5 бинарная сортировка, квиксорт, мелочи всякие

--- 9/ool/ool_lab_5/src/BinarySort.java
+++ 9/ool/ool_lab_5/src/BinarySort.java
@@ -1,32 +1,38 @@
 /**
+ * Реализация сортировки на основе бинарного дерева
  * @author lysenko
  */
-public class BinarySort
-    extends SortMethod
-{
-    /**
-     * @param array - сортируемый массив
-     */
-    public BinarySort(
-        short[] array )
-    {
-        super( array );
-    }
+public class BinarySort extends SortMethod {
 
-    /**
-     * Выполнить сотрировку массива на основе бинарного дерева
-     */
-    protected void sort()
-        throws InterruptedException
-    {
-        // TODO Auto-generated method stub
-    }
+	/**
+	 * @param array - сортируемый массив
+	 */
+	public BinarySort( short[] array ) {
+		super(array);
+	}
 
-    /**
-     * @return Имя метода
-     */
-    protected String getMethodName()
-    {
-        return "Binary tree sort";
-    }
+	/**
+	 * Выполнить сотрировку массива на основе бинарного дерева
+	 */
+	protected void sort() throws InterruptedException {
+		
+		BinaryTree tree = new BinaryTree(_array[0]);
+		
+		// ЗАПОЛНИТЬ БИНАРНОЕ ДЕРЕВО
+		for ( int i = 1; i < _array.length; i++ ) {
+			BinaryTree subTree = new BinaryTree(_array[i]);
+			tree.add(subTree);
+		}
+		
+		// ВЫПОЛНИТЬ СОРТИРОВКУ, СОХРАНИВ РЕЗУЛЬТАТ В ИСХОДНОМ МАССИВЕ
+		tree.sort(_array);
+	}
+	
+	/**
+	 * @return Имя метода
+	 */
+	protected String getMethodName(){
+		return "Binary tree sort";
+	}
+
 }
