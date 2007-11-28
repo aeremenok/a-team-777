0d02bac1ca7cd77ee698a091acd68f815da6193d
scorpion1105 <scorpion1105@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-11-28 19:36:34 +0000

    (+) Лаба №5 (*) Нереализована сотрировка деревом (*) Разобраться с быстрой сортировкой (*) Возможно нужны какие-то общие дополнения

---
+++ 9/ool/ool_lab_5/src/BinarySort.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+
+/**
+ * @author lysenko
+ *
+ */
+public class BinarySort extends SortMethod {
+
+	/**
+	 * @param array - сортируемый массив
+	 */
+	public BinarySort( short[] array ) {
+		super(array);
+	}
+
+	/**
+	 * ¬ыполнить сотрировку массива на основе бинарного дерева
+	 */
+	protected void sort() throws InterruptedException {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * @return »м€ метода
+	 */
+	protected String getMethodName(){
+		return "Binary tree sort";
+	}
+
+}
---
+++ 9/ool/ool_lab_5/src/SortMethod.java
@@ -0,0 +1,82 @@
+/**
+ * @author lysenko
+ *
+ */
+abstract class SortMethod implements Runnable {
+	
+	/**
+	 * Копия сортируемого массива
+	 */
+	protected short[] _array;
+	
+	/**
+	 * Объект потока
+	 */
+	Thread t;
+	
+	/**
+	 * @param array - сортируемый массив
+	 */
+	public SortMethod( short[] array ) {
+		
+		// СОЗДАТЬ ПОТОК
+		t = new Thread(this, getMethodName());
+		
+		System.out.println("\nThread created: " + t);
+		
+		// СОЗДАТЬ КОПИЮ ПОДЛЕЖАЩЕГО СОРТИРОВКЕ МАССИВА
+		_array = new short[array.length];
+		
+		for ( int i = 0; i < array.length; i++ )
+			_array[i] = array[i];
+		
+		printArray("initial array");
+		
+		// ЗАПУСТИТЬ ПОТОК
+		t.start();
+	}
+	
+	/**
+	 * Вывести текущее содержимое массива на экран
+	 * @param s - Строка, которая будет напечатана перед массивом  
+	 */
+	private void printArray(String s){
+		
+		synchronized (System.out) {		
+			
+			System.out.println("\n" + getMethodName() + " - " + s + ":" );
+			
+			for ( int i = 0; i < _array.length; i++ )
+				System.out.print(_array[i] + " ");
+			
+			System.out.println();		
+		}
+	}
+	
+	/**
+	 * @return Имя метода
+	 */
+	protected String getMethodName(){
+		return "No method specified.";
+	}
+	
+	/**
+	 * Выполнить сортировку
+	 */
+	abstract protected void sort() throws InterruptedException;
+	
+	/** 
+	 * Точка входа
+	 */
+	public void run(){		
+		try {
+			
+			sort();
+			
+			printArray("sorted array");
+			
+		} catch (InterruptedException e) {
+			System.out.println("Поток " + getMethodName() + " прерван!");
+		}
+	}
+}
