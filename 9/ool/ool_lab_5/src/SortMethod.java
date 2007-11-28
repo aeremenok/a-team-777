d3bb36954dcd70217d0cb88e651e26ce89b6d695
stas.sviridenko <stas.sviridenko@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-11-28 20:23:45 +0000

    ООЯИС-5 реформат кода

--- 9/ool/ool_lab_5/src/SortMethod.java
+++ 9/ool/ool_lab_5/src/SortMethod.java
@@ -1,82 +1,97 @@
 /**
  * @author lysenko
- *
  */
-abstract class SortMethod implements Runnable {
-	
-	/**
-	 * Копия сортируемого массива
-	 */
-	protected short[] _array;
-	
-	/**
-	 * Объект потока
-	 */
-	Thread t;
-	
-	/**
-	 * @param array - сортируемый массив
-	 */
-	public SortMethod( short[] array ) {
-		
-		// СОЗДАТЬ ПОТОК
-		t = new Thread(this, getMethodName());
-		
-		System.out.println("\nThread created: " + t);
-		
-		// СОЗДАТЬ КОПИЮ ПОДЛЕЖАЩЕГО СОРТИРОВКЕ МАССИВА
-		_array = new short[array.length];
-		
-		for ( int i = 0; i < array.length; i++ )
-			_array[i] = array[i];
-		
-		printArray("initial array");
-		
-		// ЗАПУСТИТЬ ПОТОК
-		t.start();
-	}
-	
-	/**
-	 * Вывести текущее содержимое массива на экран
-	 * @param s - Строка, которая будет напечатана перед массивом  
-	 */
-	private void printArray(String s){
-		
-		synchronized (System.out) {		
-			
-			System.out.println("\n" + getMethodName() + " - " + s + ":" );
-			
-			for ( int i = 0; i < _array.length; i++ )
-				System.out.print(_array[i] + " ");
-			
-			System.out.println();		
-		}
-	}
-	
-	/**
-	 * @return Имя метода
-	 */
-	protected String getMethodName(){
-		return "No method specified.";
-	}
-	
-	/**
-	 * Выполнить сортировку
-	 */
-	abstract protected void sort() throws InterruptedException;
-	
-	/** 
-	 * Точка входа
-	 */
-	public void run(){		
-		try {
-			
-			sort();
-			
-			printArray("sorted array");
-			
-		} catch (InterruptedException e) {
-			System.out.println("Поток " + getMethodName() + " прерван!");
-		}
-	}
+abstract class SortMethod
+    implements
+        Runnable
+{
+    /**
+     * Копия сортируемого массива
+     */
+    protected short[] _array;
+
+    /**
+     * Объект потока
+     */
+    Thread            t;
+
+    /**
+     * @param array - сортируемый массив
+     */
+    public SortMethod(
+        short[] array )
+    {
+
+        // СОЗДАТЬ ПОТОК
+        t = new Thread( this, getMethodName() );
+
+        System.out.println( "\nThread created: " + t );
+
+        // СОЗДАТЬ КОПИЮ ПОДЛЕЖАЩЕГО СОРТИРОВКЕ МАССИВА
+        _array = new short[array.length];
+
+        for ( int i = 0; i < array.length; i++ )
+        {
+            _array[i] = array[i];
+        }
+
+        printArray( "initial array" );
+
+        // ЗАПУСТИТЬ ПОТОК
+        t.start();
+    }
+
+    /**
+     * Вывести текущее содержимое массива на экран
+     * 
+     * @param s - Строка, которая будет напечатана перед массивом
+     */
+    private void printArray(
+        String s )
+    {
+
+        synchronized ( System.out )
+        {
+
+            System.out.println( "\n" + getMethodName() + " - " + s + ":" );
+
+            for ( short element : _array )
+            {
+                System.out.print( element + " " );
+            }
+
+            System.out.println();
+        }
+    }
+
+    /**
+     * @return Имя метода
+     */
+    protected String getMethodName()
+    {
+        return "No method specified.";
+    }
+
+    /**
+     * Выполнить сортировку
+     */
+    abstract protected void sort()
+        throws InterruptedException;
+
+    /**
+     * Точка входа
+     */
+    public void run()
+    {
+        try
+        {
+            sort();
+
+            printArray( "sorted array" );
+        }
+        catch ( InterruptedException e )
+        {
+            System.out.println( "Поток " + getMethodName() + " прерван!" );
+        }
+    }
 }
--- 9/ool/ool_lab_5/src/Test.java
+++ 9/ool/ool_lab_5/src/Test.java
@@ -2,58 +2,64 @@ import java.util.Random;
 
 /**
  * @author lysenko
- *
  */
-public class Test {
-	
-	/**
+public class Test
+{
+    /**
      * Äàò÷èê ñëó÷àéíûõ ÷èñåë
      */
-    private static Random _random      = null;
-    
+    private static Random _random          = null;
+
     /**
      * Ìàêñèìàëüíàÿ äëèíà ìàññèâà
      */
-    private static int MAX_ARRAY_LENGTH = 255;
+    private static int    MAX_ARRAY_LENGTH = 255;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-		int arrayLength = 0;
-		
-		// ÈÍÈÖÈÀËÈÇÈĞÎÂÀÒÜ ÄÀÒ×ÈÊ ÑËÓ×ÀÉÍÛÕ ×ÈÑÅË
-		_random = new Random( System.currentTimeMillis() );
-		
-		// ÇÀÄÀÒÜ ÄËÈÍÓ ÌÀÑÑÈÂÀ
-		if ( args.length == 1 )
-			arrayLength = Integer.parseInt(args[0]);
-		else 
-			arrayLength = (int)(_random.nextDouble() * MAX_ARRAY_LENGTH) + 1;
-			
-		// ÑÎÇÄÀÒÜ ÌÀÑÑÈÂ
-		short[] toSortArray = new short[arrayLength];
-		
-		// ÈÍÈÖÈÀËÈÇÈĞÎÂÀÒÜ ÌÀÑÑÈÂ ÑËÓ×ÀÉÍÛÌÈ ×ÈÑËÀÌÈ
-		for ( int i = 0; i < toSortArray.length; i++ )
-			toSortArray[i] = (short)_random.nextInt();
-		
-		// ÂÛÂÅÑÒÈ ÈÑÕÎÄÍÛÉ ÌÀÑÑÈÂ ÍÀ İÊĞÀÍ
-		System.out.println("\nInitial array (" + arrayLength + " elements):");
-		for ( int i = 0; i < toSortArray.length; i++ )
-			System.out.print(toSortArray[i] + " ");
-		System.out.println();
-		
-		// ÇÀÏÓÑÒÈÒÜ ÏÎÒÎÊÈ ÑÎĞÒÈĞÎÂÊÈ
-		synchronized (toSortArray) {
-			
-			new ShellSort(toSortArray);
-			new HoareSort(toSortArray);
-			new BubbleSort(toSortArray);
-			new BinarySort(toSortArray);
-			
-		}
-	}
+    /**
+     * @param args
+     */
+    public static void main(
+        String[] args )
+    {
+        int arrayLength = 0;
 
+        // ÈÍÈÖÈÀËÈÇÈĞÎÂÀÒÜ ÄÀÒ×ÈÊ ÑËÓ×ÀÉÍÛÕ ×ÈÑÅË
+        _random = new Random( System.currentTimeMillis() );
+
+        // ÇÀÄÀÒÜ ÄËÈÍÓ ÌÀÑÑÈÂÀ
+        if ( args.length == 1 )
+        {
+            arrayLength = Integer.parseInt( args[0] );
+        }
+        else
+        {
+            arrayLength = (int) (_random.nextDouble() * MAX_ARRAY_LENGTH) + 1;
+        }
+
+        // ÑÎÇÄÀÒÜ ÌÀÑÑÈÂ
+        short[] toSortArray = new short[arrayLength];
+
+        // ÈÍÈÖÈÀËÈÇÈĞÎÂÀÒÜ ÌÀÑÑÈÂ ÑËÓ×ÀÉÍÛÌÈ ×ÈÑËÀÌÈ
+        for ( int i = 0; i < toSortArray.length; i++ )
+        {
+            toSortArray[i] = (short) _random.nextInt();
+        }
+
+        // ÂÛÂÅÑÒÈ ÈÑÕÎÄÍÛÉ ÌÀÑÑÈÂ ÍÀ İÊĞÀÍ
+        System.out.println( "\nInitial array (" + arrayLength + " elements):" );
+        for ( short element : toSortArray )
+        {
+            System.out.print( element + " " );
+        }
+        System.out.println();
+
+        // ÇÀÏÓÑÒÈÒÜ ÏÎÒÎÊÈ ÑÎĞÒÈĞÎÂÊÈ
+        synchronized ( toSortArray )
+        {
+            new ShellSort( toSortArray );
+            new HoareSort( toSortArray );
+            new BubbleSort( toSortArray );
+            new BinarySort( toSortArray );
+        }
+    }
 }
