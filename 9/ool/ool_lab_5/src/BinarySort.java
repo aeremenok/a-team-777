84b6d31a477de802fa7f53d605d2ee7a13449a05
stas.sviridenko <stas.sviridenko@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-11-29 05:29:33 +0000

    ООЯИС-5 реформат

--- 9/ool/ool_lab_5/src/BinarySort.java
+++ 9/ool/ool_lab_5/src/BinarySort.java
@@ -1,38 +1,47 @@
 /**
  * Реализация сортировки на основе бинарного дерева
+ * 
  * @author lysenko
  */
-public class BinarySort extends SortMethod {
+public class BinarySort
+    extends SortMethod
+{
 
-	/**
-	 * @param array - сортируемый массив
-	 */
-	public BinarySort( short[] array ) {
-		super(array);
-	}
+    /**
+     * @param array - сортируемый массив
+     */
+    public BinarySort(
+        short[] array )
+    {
+        super( array );
+    }
 
-	/**
-	 * Выполнить сотрировку массива на основе бинарного дерева
-	 */
-	protected void sort() throws InterruptedException {
-		
-		BinaryTree tree = new BinaryTree(_array[0]);
-		
-		// ЗАПОЛНИТЬ БИНАРНОЕ ДЕРЕВО
-		for ( int i = 1; i < _array.length; i++ ) {
-			BinaryTree subTree = new BinaryTree(_array[i]);
-			tree.add(subTree);
-		}
-		
-		// ВЫПОЛНИТЬ СОРТИРОВКУ, СОХРАНИВ РЕЗУЛЬТАТ В ИСХОДНОМ МАССИВЕ
-		tree.sort(_array);
-	}
-	
-	/**
-	 * @return Имя метода
-	 */
-	protected String getMethodName(){
-		return "Binary tree sort";
-	}
+    /**
+     * Выполнить сотрировку массива на основе бинарного дерева
+     */
+    protected void sort()
+        throws InterruptedException
+    {
+
+        BinaryTree tree = new BinaryTree( _array[0] );
+
+        // ЗАПОЛНИТЬ БИНАРНОЕ ДЕРЕВО
+        for ( int i = 1; i < _array.length; i++ )
+        {
+            BinaryTree subTree = new BinaryTree( _array[i] );
+            tree.add( subTree );
+        }
+
+        // ВЫПОЛНИТЬ СОРТИРОВКУ, СОХРАНИВ РЕЗУЛЬТАТ В ИСХОДНОМ МАССИВЕ
+        tree.sort( _array );
+    }
+
+    /**
+     * @return Имя метода
+     */
+    protected String getMethodName()
+    {
+        return "Binary tree sort";
+    }
 
 }
