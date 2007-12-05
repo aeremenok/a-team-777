db8e807db59d14a3ad44bd0186c9b80a802685ee
stas.sviridenko <stas.sviridenko@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-12-05 14:03:18 +0000

    СТ-2 сделаны комментарии, убрано лишнее

--- 9/st/ssv_labs/ssv_st_lab_2/npc/npc.cpp
+++ 9/st/ssv_labs/ssv_st_lab_2/npc/npc.cpp
@@ -1,21 +1,8 @@
-// npc.cpp : Defines the entry point for the console application.
-//
 
 #include "stdafx.h"
 
-/*
-int main(int argc, char* argv[])
-{
-	printf("Hello World!\n");
-	return 0;
-}
-*/
-
 #include <windows.h>
 #include <stdio.h>
-
-#define BUFSIZE 1024
-#define PIPE_TIMEOUT 5000
 
 int main()
 {
@@ -23,31 +10,44 @@ int main()
     BOOL flg;
     DWORD dwWrite;
     char szPipeUpdate[200];
-    hFile = CreateFile("\\\\.\\pipe\\SamplePipe", GENERIC_WRITE,
-        0, NULL, OPEN_EXISTING,
-        0, NULL);
 
-    strcpy(szPipeUpdate,"Data from Named Pipe client for createnamedpipe");
+	// янгдю╗л тюик
+    hFile = CreateFile(
+        "\\\\.\\pipe\\SsvPipe", // хлъ йюмюкю
+        GENERIC_WRITE,          // рхо днярсою: гюохяэ
+        0,                      // пюгдекемхе днярсою: мер
+        NULL,                   // аегноюямнярэ: мер
+        OPEN_EXISTING,          // нрйпшбюрэ ясыеярбсчыхи тюик
+        0,                      // ткюцх х юрпхасрш: мер
+        NULL);                  // ьюакнм: мер
 
+	// йнохпсел ярпнйс дкъ оепедювх
+    strcpy(szPipeUpdate, "!!!Hello, Pipes!!!");
+
+	// йюмюк ме ясыеярбсер ?
     if(hFile == INVALID_HANDLE_VALUE)
     {
-        printf("CreateFile failed for Named Pipe client\n" );
+		printf("ERROR: Named Pipe Client: No such pipe exists!\n" );
     }
     else
     {
+		// гюохяэ ярпнйх б йюмюк
         flg = WriteFile(hFile, szPipeUpdate, strlen(szPipeUpdate), &dwWrite, NULL);
         
+		// гюохяэ ме сдюкюяэ ?
         if (FALSE == flg)
         {
-            printf("WriteFile failed for Named Pipe client\n");
+			printf("ERROR: Named Pipe Client: Cannot write to pipe!\n");
         }
         else
         {
-            printf("WriteFile succeeded for Named Pipe client\n");
+			printf("INFO:  Named Pipe Client: Successfull write into pipe!\n");
         }
         
+		// гюйпшбюел йюмюк
         CloseHandle(hFile);
     }
 
+	// гюбепьюел пюанрс
 	return 0;
 }
