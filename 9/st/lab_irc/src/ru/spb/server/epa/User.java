20ab4bc585f18e8cb41d9e9e2be16e8be00c93f3
emmanpavel <emmanpavel@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-12-09 18:27:56 +0000

    First implementation of irc server. working commands are "nick" "user" "quit"

---
+++ 9/st/lab_irc/src/ru/spb/server/epa/User.java
@@ -0,0 +1,29 @@
+package epa;
+
+/**
+ * User: Павел
+ * Date: 08.12.2007
+ * Time: 21:23:21
+ * for easy user managemant
+ */
+public class User {
+
+    private String login;
+    private String password;
+
+
+
+    public User(String password, String login) {
+        this.password = password;
+        this.login = login;
+    }
+
+
+    public boolean equals(Object obj) {
+        if(! (obj instanceof User)) return false;
+        User u = (User) obj;
+        return this.login != null && u.login != null &&
+                this.login.equals(u.login);
+
+    }
+}
