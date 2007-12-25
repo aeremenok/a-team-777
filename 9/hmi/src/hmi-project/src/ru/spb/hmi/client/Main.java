8ef1390094f5b16593cfe0850f6abbca3a2532f6
eav1986 <eav1986@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-12-25 00:38:22 +0000

    ЧМВ-К набросан интерфейс

--- 9/hmi/src/hmi-project/src/ru/spb/hmi/client/Main.java
+++ 9/hmi/src/hmi-project/src/ru/spb/hmi/client/Main.java
@@ -1,12 +1,9 @@
 package ru.spb.hmi.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.Widget;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -15,36 +12,80 @@ public class Main
     implements
         EntryPoint
 {
-    private Button          clickMeButton;
 
     private DOCServiceAsync _service = DOCService.Util.getInstance();
 
     public void onModuleLoad()
     {
         RootPanel rootPanel = RootPanel.get();
+        rootPanel.setTitle( "DocRedactor" );
 
-        clickMeButton = new Button();
-        rootPanel.add( clickMeButton );
-        clickMeButton.setText( "Click me!" );
-        clickMeButton.addClickListener( new ClickListener()
         {
-            public void onClick(
-                Widget sender )
-            {
-                _service.getString( new AsyncCallback()
-                {
-                    public void onFailure(
-                        Throwable caught )
-                    {
-                    }
+            final MenuBar menuBar = new MenuBar();
+            rootPanel.add( menuBar, 0, 0 );
+            menuBar.setWidth( "100%" );
 
-                    public void onSuccess(
-                        Object result )
+            final MenuBar menuBar_1 = new MenuBar( true );
+
+            {
+                menuBar_1.addItem( "Load", (Command) null );
+            }
+
+            {
+                menuBar_1.addItem( "Save", (Command) null );
+            }
+
+            {
+                menuBar_1.addItem( "<hr>", true, (Command) null );
+            }
+
+            {
+                menuBar_1.addItem( "PrintPreview", (Command) null );
+            }
+
+            {
+                menuBar.addItem( "Document", menuBar_1 );
+            }
+
+            final MenuBar menuBar_2 = new MenuBar( true );
+
+            {
+                menuBar_2.addItem( "Copy", (Command) null );
+            }
+
+            {
+                menuBar_2.addItem( "Paste", (Command) null );
+            }
+
+            {
+                menuBar.addItem( "Edit", menuBar_2 );
+            }
+
+            final MenuBar menuBar_3 = new MenuBar( true );
+
+            {
+                menuBar.addItem( "View", menuBar_3 );
+            }
+
+            final MenuBar menuBar_4 = new MenuBar( true );
+
+            {
+                menuBar_4.addItem( "Index", (Command) null );
+            }
+
+            {
+                menuBar_4.addItem( "About", new Command()
+                {
+                    public void execute()
                     {
-                        Window.alert( (String) result );
+                        AboutDialog.getInstance().onModuleLoad();
                     }
                 } );
             }
-        } );
+
+            {
+                menuBar.addItem( "Help", menuBar_4 );
+            }
+        }
     }
 }
