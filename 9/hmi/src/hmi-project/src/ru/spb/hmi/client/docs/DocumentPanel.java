aaaede3cb716a63a8cbe7e80bfc9b7e41dc9c47a
eav1986 <eav1986@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2007-12-26 22:09:56 +0000

    ЧМВ-К простейшая логика загрузки и редактирования

--- 9/hmi/src/hmi-project/src/ru/spb/hmi/client/docs/DocumentPanel.java
+++ 9/hmi/src/hmi-project/src/ru/spb/hmi/client/docs/DocumentPanel.java
@@ -1,21 +1,30 @@
 package ru.spb.hmi.client.docs;
 
+import ru.spb.hmi.client.DOCService;
+import ru.spb.hmi.client.DOCServiceAsync;
 import ru.spb.hmi.client.Unloadable;
+import ru.spb.hmi.client.popups.WaitingPopup;
 
-import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public abstract class DocumentPanel
-    extends FlexTable
+    extends VerticalPanel
     implements
         Unloadable
 {
-    private RootPanel rootPanel;
-    protected String  xmlContent;
+    private RootPanel              rootPanel;
+    protected String               xmlContent;
 
-    public DocumentPanel()
+    private static DOCServiceAsync _service = DOCService.Util.getInstance();
+
+    public DocumentPanel(
+        String id )
     {
         super();
+        WaitingPopup.wait( "preparing document" );
+        // готовим док-т к работе
+        _service.prepareDoc( id, new NotifyCallBack( "document prepared" ) );
     }
 
     public void onModuleLoad()
