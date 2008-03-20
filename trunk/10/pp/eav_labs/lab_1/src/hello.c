079778d0357250cfaea0f4918d71c4b6f67a39b0
eav1986 <eav1986@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2008-03-20 20:11:09 +0000

    ПП1: более сложная программка ПП2: набросок программы

--- trunk/10/pp/eav_labs/lab_1/src/hello.c
+++ trunk/10/pp/eav_labs/lab_1/src/hello.c
@@ -1,5 +1,33 @@
-#include <stdlib.h>
+#include <stdio.h> 
+#include <string.h> 
+#include <malloc.h>
+#include <stdlib.h> 
 
-int main(){
-	system("sh hello.sh");
+int main(int argc, char* argv[]) 
+{
+    char* buf; 
+    int bufsize = 0; 
+    int i;
+
+    if (argc < 2) return 0; 
+
+    for (i = 1; i < argc; i++ ) 
+    {
+        bufsize += strlen(argv[i]); 
+    }
+
+    buf = (char*) malloc( sizeof(char) * ( bufsize + (argc-1) ) ); 
+    strcpy(buf, ""); 
+
+    for ( i = 1; i < argc; i++ )
+    {
+        strcat(buf, argv[i]);
+        strcat(buf, " ");
+    }
+
+    printf("running: %s\n", buf); 
+
+    system( buf );
+
+    return 0;
 }
