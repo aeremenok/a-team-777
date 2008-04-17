c6b555ca7caec30adf3de0598461fb72567dc8cc
eav1986 <eav1986@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2008-04-17 20:47:19 +0000

    ПП Л1 обновлен отчет и программа

--- trunk/10/pp/eav_labs/lab_1/src/hello.c
+++ trunk/10/pp/eav_labs/lab_1/src/hello.c
@@ -1,33 +1,43 @@
-#include <stdio.h> 
-#include <string.h> 
-#include <malloc.h>
-#include <stdlib.h> 
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[]) 
-{
-    char* buf; 
-    int bufsize = 0; 
+int main(int argc, char* argv[]){
+
+    char* buf;
+    int bufsize = 0;
     int i;
 
-    if (argc < 2) return 0; 
+	if (argc < 2)
+	{
+	    printf("use > 2 args\n");
+	    return -1;
+	}
 
-    for (i = 1; i < argc; i++ ) 
-    {
-        bufsize += strlen(argv[i]); 
-    }
+	printf("argc %d\n", argc);
 
-    buf = (char*) malloc( sizeof(char) * ( bufsize + (argc-1) ) ); 
-    strcpy(buf, ""); 
+	for (i = 1; i < argc; i++)
+	{
+	    bufsize+= strlen(argv[i]) + 1;
+	}
 
-    for ( i = 1; i < argc; i++ )
-    {
-        strcat(buf, argv[i]);
-        strcat(buf, " ");
-    }
+	printf("bufsize %d\n", bufsize);
+	buf = (char*)malloc( sizeof(char) * bufsize);
+	printf("malloc\n");
+	strcpy(buf, "");
 
-    printf("running: %s\n", buf); 
+	printf("buf %s", buf);
 
-    system( buf );
+	for (i = 1; i < argc; i++)
+	{
+	    strcat(buf, argv[i]);
+	    strcat(buf, " ");
+	}
 
-    return 0;
-}
+	printf ("running %s\n", buf);
+
+	system(buf);
+
+	return 0;
+} 
