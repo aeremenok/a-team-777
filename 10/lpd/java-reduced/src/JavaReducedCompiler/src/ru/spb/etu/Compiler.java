56e446633968c88698591a3748f83bb1c3a98c96
eav1986 <eav1986@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2008-05-03 17:41:00 +0000

    РЯП основа проекта

---
+++ 10/lpd/java-reduced/src/JavaReducedCompiler/src/ru/spb/etu/Compiler.java
@@ -0,0 +1,20 @@
+package ru.spb.etu;
+
+/**
+ * запускает компил€цию
+ * 
+ * @author eav
+ */
+public class Compiler
+{
+    /**
+     * @param args
+     */
+    public static void main(
+        String[] args )
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
