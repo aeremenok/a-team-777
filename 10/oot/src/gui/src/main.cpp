d934726a71c38bb26e0a94611d74d17121a4d2d3
Pavel.Zubarev <Pavel.Zubarev@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2008-05-10 18:28:19 +0000

    gui sample

---
+++ 10/oot/src/gui/src/main.cpp
@@ -0,0 +1,51 @@
+#include <getopt.h>
+#include <unistd.h>
+#include <QtGui/QApplication>
+#include <QtGui/QLabel>
+#include <QtCore/QTextCodec>
+#include "MainWindow.h"
+void printUsage()
+{
+  printf("  Супер пупер программа по ООТ \n"
+         "  -h  --help\n");
+}
+
+int main(int argc, char *argv[])
+{
+  option opts[]=
+  {
+    {"help",optional_argument,NULL,'h'},
+    {0}
+  };
+
+
+  int indx,opt; 
+
+  const char *pConfig=NULL;
+
+  while((opt=getopt_long(argc,argv,"hc:",opts,&indx))!=-1)
+  {
+    switch(opt)
+    {
+      case 'c':
+      pConfig=optarg;
+      break;
+
+      case 'h':
+      printUsage();
+      return 0;
+    }
+  }
+
+  QTextCodec *rusCodec = QTextCodec::codecForName("KOI8-R");
+  QTextCodec::setCodecForTr(rusCodec);
+
+  QApplication app(argc, argv);
+
+  CMainWindow mainFrame;
+  
+  mainFrame.showMaximized();
+  
+  return app.exec();
+}; 
+
