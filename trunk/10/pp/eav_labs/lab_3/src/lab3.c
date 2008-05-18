58d2923792c6a5c3aeb35df66eaf20190207a24e
eav1986 <eav1986@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2008-05-18 18:14:18 +0000

    ПП работающая л/р Harry,Tony&Mary

--- trunk/10/pp/eav_labs/lab_3/src/lab3.c
+++ trunk/10/pp/eav_labs/lab_3/src/lab3.c
@@ -1,94 +1,147 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
- 
+
 #include <sys/link.h>
 #include <sys/root.h>
 #include <sys/sys_rpc.h>
 #include <sys/thread.h>
- 
+
 const int STACK_SIZE = 4096;
-char answer[100];
-RootProc_t* root;
-    
+int myprocid;
+	
 int harry(void)
 {
-    int myprocid, error;
-    LinkCB_t* TonyLink;
-    int  i;
-    char* question[] = {
-        "how r u?",
-        "what do u think of parix?"
-    };
-    
-    myprocid = root->MyProcID;
-    TonyLink = MakeLink( myprocid, 1234, &error);
-    if (!TonyLink)    
-    {  
-        LogError(EC_ERROR, "harry", "errcode %d", error );
-        exit(1);
-    }
-    
-    for (i = 0; i < 2; i++){
-        printf("talk to tony %s\n", question[i]);
-        SendLink( TonyLink, (byte*)question[i], strlen(question[i]) );
-        RecvLink( TonyLink, (byte*)answer, 1024 );
-    }
+char answer[100];
+	int error;
+	LinkCB_t* TonyLink, *MaryLink;
+	int  i;
+	char* question[] = {
+		"how are you?",
+		"what do you think of parix?"
+	};
 
-    return 0;
+		TonyLink = MakeLink( myprocid, 1234, &error);
+		if (!TonyLink)	
+		{ 
+			LogError(EC_ERROR, "harry", "errcode %d", error );
+			printf("tony link error");
+			exit(1);
+		}
+
+		MaryLink = MakeLink( myprocid, 1234, &error);
+		if (!MaryLink)	
+		{ 
+			LogError(EC_ERROR, "mary", "errcode %d", error );
+			printf("mary link error");
+			exit(1);
+		}
+	
+		for (i = 0; i < 2; i++){
+			SendLink( TonyLink, (byte*)question[i], strlen(question[i])+1);
+			SendLink( MaryLink, (byte*)question[i], strlen(question[i])+1);
+			printf("harry said: %s\n",question[i]);
+
+
+			RecvLink( MaryLink, (byte*)answer, 1024 );
+			printf("mary said: %s\n",answer);
+			RecvLink( TonyLink, (byte*)answer, 1024 );
+			printf("tony said: %s\n",answer);
+		}
+
+	return 0;
 }
- 
+
 int tony(void)
 {
-    int myprocid, error;
-    LinkCB_t* HarryLink;
-    int  i;
-    char* question[] = {
-        "how r u?",
-        "what do u think of parix?"
-    };
- 
-    myprocid = root->MyProcID;
-    HarryLink = MakeLink(myprocid, 1234, &error);
-    if (!HarryLink)
-    {  
-        LogError(EC_ERROR, "harry", "errcode %d", error );
-        exit(1);
-    }
-    
-    for (i = 0; i < 2; i++){
-        printf("talk to harry %s\n", question[i]);
-        SendLink( HarryLink, (byte*)question[i], strlen(question[i]) );
-        RecvLink( HarryLink, (byte*)answer, 1024 );
-    }
+char answer[100];
+	int error;
+	LinkCB_t* HarryLink;
+	int  i;
+	char* question[] = {
+		"fine",
+		"it works"
+	};
 
-    return 0;
+	printf(" tony working  %d \r\n", myprocid);
+	HarryLink = GetLink(myprocid, 1234, &error);
+
+		if (!HarryLink)
+		{ 
+			LogError(EC_ERROR, "harry", "errcode %d", error );
+			exit(1);
+		}
+	
+		for (i = 0; i < 2; i++){
+			RecvLink( HarryLink, (byte*)answer, 1024 );
+			SendLink( HarryLink, (byte*)question[i], strlen(question[i])+1 );
+		}
+
+	return 0;
 }
- 
+
+int mary(void)
+{
+char answer[100];
+	int error;
+	LinkCB_t* HarryLink;
+	int  i;
+	char* question[] = {
+		"could be better",
+		"it's too buggy"
+	};
+
+	printf(" mary working  %d \r\n", myprocid);
+	HarryLink = GetLink(myprocid, 1234, &error);
+
+		if (!HarryLink)
+		{ 
+			LogError(EC_ERROR, "harry", "errcode %d", error );
+			exit(1);
+		}
+	
+		for (i = 0; i < 2; i++){
+			RecvLink( HarryLink, (byte*)answer, 1024 );
+			SendLink( HarryLink, (byte*)question[i], strlen(question[i])+1 );
+		}
+
+	return 0;
+}
+
 int main(int argc, char* argv[])
 {
-    Thread_t* tHarry;
-    Thread_t* tTony;
-    int error, result;
+	Thread_t* tHarry;
+	Thread_t* tTony;
+	Thread_t* tMary;
+	int error, result;
 
-    root = GET_ROOT()->ProcRoot;
+	myprocid = GET_ROOT()->ProcRoot->MyProcID;
 
-    tHarry = StartThread(  harry, STACK_SIZE, &error, 0 );
-    if (!tHarry)
-    {  
-        LogError(EC_ERROR, "harry", "errcode %d", error );
-        exit(1);
-    }
+	tHarry = StartThread(  harry, STACK_SIZE, &error, 0 );
+	if (tHarry==NULL)
+	{ 
+		LogError(EC_ERROR, "harry", "errcode %d", error );
+		printf("harry error");
+		exit(1);
+	}
 
-    tTony = StartThread( tony, STACK_SIZE, &error, 0 );
-    if (!tTony)
-    {  
-        LogError(EC_ERROR, "tony", "errcode %d", error );
-        exit(1);
-    }
+	tTony = StartThread( tony, STACK_SIZE, &error, 0 );
+	if (tTony == NULL)
+	{ 
+		LogError(EC_ERROR, "tony", "errcode %d", error );
+		exit(1);
+	}
 
-    WaitThread(tHarry, &result);
-    WaitThread(tTony, &result);
-    
-    return 0;
+	tMary = StartThread( mary, STACK_SIZE, &error, 0 );
+	if (tTony == NULL)
+	{ 
+		LogError(EC_ERROR, "mary", "errcode %d", error );
+		exit(1);
+	}
+
+	WaitThread(tHarry, &result);
+	WaitThread(tTony, &result);
+	WaitThread(tMary, &result);
+
+	return 0;
 }
