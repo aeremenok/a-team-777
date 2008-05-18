cfa11108ec714b512d18d70bbe0b8ae46c917e83
eav1986 <eav1986@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2008-05-18 13:08:07 +0000

    РПО написан getArtistsByMuseum

--- 10/dbms_swd/projects/Gallery/src/ru/spb/etu/server/model/DbMuseum.java
+++ 10/dbms_swd/projects/Gallery/src/ru/spb/etu/server/model/DbMuseum.java
@@ -1,17 +1,32 @@
 package ru.spb.etu.server.model;
 
+import java.util.List;
+
 import org.apache.cayenne.DataObjectUtils;
-import org.apache.cayenne.ObjectContext;
+import org.apache.cayenne.access.DataContext;
+import org.apache.cayenne.exp.Expression;
+import org.apache.cayenne.exp.ExpressionFactory;
+import org.apache.cayenne.query.SelectQuery;
 
 import ru.spb.etu.server.model.auto._DbMuseum;
 
-public class DbMuseum extends _DbMuseum {
-	public Integer  getId(){
-		return DataObjectUtils.intPKForObject(this);
-	}
-	
-	
+public class DbMuseum
+    extends _DbMuseum
+{
+    public Integer getId()
+    {
+        return DataObjectUtils.intPKForObject( this );
+    }
+
+    /**
+     * @param context
+     * @return все произведения искусства в этом музее
+     */
+    public List<DbMasterpiece> getMasterpieces(
+        DataContext context )
+    {
+        Expression qualifier = ExpressionFactory.matchExp( DbMasterpiece.MY_MUSEUM_PROPERTY, getId() );
+        SelectQuery select = new SelectQuery( DbMasterpiece.class, qualifier );
+        return context.performQuery( select );
+    }
 }
-
-
-
