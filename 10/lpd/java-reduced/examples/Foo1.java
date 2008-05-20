cd629e8ac6a4dcbf75e146a4fbe98554ce1af225
eav1986 <eav1986@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2008-05-20 21:32:55 +0000

    РЯП синтаксический разбор бОльшей части, коммит перед очисткой от проверок сторонних разработчиков

--- 10/lpd/java-reduced/examples/Foo1.java
+++ 10/lpd/java-reduced/examples/Foo1.java
@@ -3,27 +3,26 @@
 public class Foo extends Fufel
 {
   
-  public final static bool a;
+  public final static boolean a;
   public Foo() {}
-  public static Foo1 main(
+  public static Foo main(
         int args, String argv )
   {
-  	  int a = 6, b=4;
-  	  add(6>=5,a);
-  	  b = 966;
-  	  super.helloWorld(7!=6, st, "ava");
-  	  if(a > 5) 
-  	  {
-  	  	  bool b = 5==Foo4.Hello("GOOD");
-  	  	  return;
-  	  }
-  	 return 6<=3;
-  	 {
-  	 	 System.out.println( c );
-  	 	 return 7==4;
-  	 }
+    int a = 6;
+    int b = 4;
+    add(6>=5,zzz);
+    b = 966;
+    super.helloWorld(7!=6, st, "ava");
+    if(a > 5) 
+    {
+        boolean b = 5==Foo4.Hello("GOOD");
+        return;
+    }
+    return 6<=3;
+    {
+        System.out.println( c );
+        return 7==4;
+    }
   }
-    
-		// todo что-то вызвать
-    
+        // todo что-то вызвать
 }
