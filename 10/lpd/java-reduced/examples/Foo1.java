dd676478fcc8db326fe8f66f18e32711f3635b18
eav1986 <eav1986@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2008-05-21 12:47:37 +0000

    РЯП различаются объявление с инциализацией и присвоения

--- 10/lpd/java-reduced/examples/Foo1.java
+++ 10/lpd/java-reduced/examples/Foo1.java
@@ -1,33 +1,9 @@
-public class Foo1 extends Bar
-{
-  public byte a;
-  public Foo1() {
-	super.helloWorld(7!=6, true, "ava");
-	int ba = 0;
-  }
-  public void main()
-  {
-    int a = 6;
-    int b = 4;
-    add(6>=5,a);
-    b = 966;
-    
-    if(a > 5) 
-    {
-        boolean c = 5==super.Hello("GOOD");
-    }
-    
-    {
-        System.out.println( b );
-    }
-  }
-        // todo что-то вызвать
-}
+public class Foo1{
 
-class Bar{
-int Hello(String str){
-	return 0;
-}
-void helloWorld(boolean b, boolean c, String a){ }
-void add(boolean b, int a){}
+    public void main(){
+        int a = 0;
+        a = 1;
+        float b = 777;
+    }
+
 }
