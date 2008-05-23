ad7cafbcfa0e89334ca63ace264abe3ee349bcfb
scorpion1105 <scorpion1105@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2008-05-23 14:40:55 +0000

    (*) [OOT_MOZG] Выкинуто лишнее

--- trunk/10/oot_Mozg/ExtTools/excptns.h
+++ trunk/10/oot_Mozg/ExtTools/excptns.h
@@ -22,55 +22,6 @@ namespace FSG  //! FreeStyle Group
 #endif
          throw e;
    }
-/*
-// ==========================================================================
-   enum  {  ERR_GENERIC = 1   };                         // ќбщий код ошибки
-// ==========================================================================
-
-class FSG_CMMN_CLASS EXCEPTION
-{
-public:
-   EXCEPTION(long erc = ERR_GENERIC, bool adel = true) : 
-      err_code(erc), AutoDelete(adel)                      {};
-   virtual ~EXCEPTION()   {};
-   // -----------------------------------------------------------------------
-   virtual bool GetErrorMessage(char* text, int len) const { return false; };
-   virtual int  ReportError(lpcstr caption) const;
-   // -----------------------------------------------------------------------
-   void         Delete()   {if (AutoDelete)   delete this;   };
-   // -----------------------------------------------------------------------
-   long err_code;                                              // код ошибки
-private:
-   bool AutoDelete;
-};
-*/
-
-// ==========================================================================
-//                            √≈Ќ≈–ј÷»я »— Ћё„≈Ќ»я
-
-//! \todo ”далить старые FSG_THROW из excptns.h
-/*
-inline void FSG_THROW(EXCEPTION* e)
-{
-#if (defined(_DEBUG) || defined(DEBUG))
-   CONFIRM(e);
-   //e->ReportError(NULL);
-   bool x = false;
-   if (x)   // можно под отладчиком изменить значение переменной и вернутьс€
-   {        // в вызвавший функцию код
-      e->Delete();
-      return;
-   }
-   else
-#endif
-      throw e;
-};
-
-inline void FSG_THROW(EXCEPTION& e)
-{
-   FSG_THROW(&e);
-};
-*/
 }  // end of namespace FSG
 
 #endif // __EXCPTNS_H
