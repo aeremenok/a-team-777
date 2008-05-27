e39000b535d1d0d3dbba235bc12b95bc0d4a4a15
eav1986 <eav1986@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2008-05-27 12:48:18 +0000

    РЯП откомментирован код

--- trunk/10/lpd/java-reduced/src/java/src/ru/spb/etu/Args.java
+++ trunk/10/lpd/java-reduced/src/java/src/ru/spb/etu/Args.java
@@ -2,7 +2,11 @@ package ru.spb.etu;
 
 import org.apache.bcel.generic.Type;
 
-// обертка формальных параметров
+/**
+ * обертка формальных параметров
+ * 
+ * @author eav
+ */
 public class Args
 {
     Type[]   argTypes = null;
