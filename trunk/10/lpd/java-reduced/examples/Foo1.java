99bb13b04de5f6d6e42fef8b65f657dfaad5849e
eav1986 <eav1986@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2008-05-29 21:17:15 +0000

    РЯП решена проблема обращения к this

--- trunk/10/lpd/java-reduced/examples/Foo1.java
+++ trunk/10/lpd/java-reduced/examples/Foo1.java
@@ -1,51 +1,17 @@
-//import java.util.Vector;
-/*
-public class Entry{
-    public String value;
-    public Entry next;
-    public Entry prev;
-}
-
-public class List{
-    Entry head;
-    int length;
-    
-    List(){
-        head = null;
-        length = 0;
-    }
-    
-    void add(String val)
-    {
-        Entry e = new Entry();
-        
-    }
-    
-    Entry getLast()
-    {
-        //while()
-        return null;
-    }
-}
-*/
-
 public class Foo1 
 {
-//    public static void main(String[] args){ new Foo1().main(); }
+    //public static void main(String[] args){ new Foo1().main(); }
+    Foo1 instance;
   
     Foo1 get(){
-        return null;
+        instance = this;
+        return instance;
     }
   
     public void main()
     {
-        Foo1 a = null;
-        Foo1 b = new Foo1();
-        if ((a!=b))
-        {
-            System.out.println(4554);
-        }
-        
+        Foo1 f = new Foo1();
+        System.out.println(f.get());
     }
 
 }
