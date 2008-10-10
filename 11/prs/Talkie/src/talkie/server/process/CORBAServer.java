e5225e360392d4a79527a1d2ce5b6718bcd137a1
stas.sviridenko <stas.sviridenko@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2008-10-10 10:19:55 +0000

    ПРС реализована динамическая подгрузка реализованных протоколов при запуске приложения

---
+++ 11/prs/Talkie/src/talkie/server/process/CORBAServer.java
@@ -0,0 +1,10 @@
+package talkie.server.process;
+
+public class CORBAServer
+    implements
+        Runnable
+{
+    public void run()
+    {
+    }
+}
---
+++ 11/prs/Talkie/src/talkie/server/process/RMIServer.java
@@ -0,0 +1,10 @@
+package talkie.server.process;
+
+public class RMIServer
+    implements
+        Runnable
+{
+    public void run()
+    {
+    }
+}
