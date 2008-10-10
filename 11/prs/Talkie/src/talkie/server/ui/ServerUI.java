b16c7d9655c24396bd5a9169432687adb89d74d8
stas.sviridenko <stas.sviridenko@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2008-10-10 08:13:09 +0000

    ПРС меню

--- 11/prs/Talkie/src/talkie/server/ui/ServerUI.java
+++ 11/prs/Talkie/src/talkie/server/ui/ServerUI.java
@@ -2,17 +2,20 @@ package talkie.server.ui;
 
 import java.awt.BorderLayout;
 
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 import talkie.common.ui.MyFrame;
 import talkie.server.Server;
 
 public class ServerUI
+    extends MyFrame
     implements
         Runnable
 {
-    private MyFrame      frame = new MyFrame();
-    private final Server server;
+    private static final String EXIT = "exit";
+    private final Server        server;
 
     public ServerUI(
         Server server )
@@ -22,10 +25,41 @@ public class ServerUI
 
     public void run()
     {
-        frame.setLayout( new BorderLayout() );
+        setLayout( new BorderLayout() );
+        initMenuBar();
 
-        JMenuBar mb = new JMenuBar();
-        frame.setJMenuBar( mb );
+        display();
+    }
 
+    private void initMenuBar()
+    {
+        // Файл
+        JMenu mFile = new JMenu( "Файл" );
+
+        JMenuItem mFileExit = new JMenuItem( "Выход" );
+        mFile.add( mFileExit );
+
+        // Протоколы
+        JMenu mProtocols = new JMenu( "Протоколы" );
+        JMenuItem mProtUdp = new JMenuItem( "UDP" );
+        JMenuItem mProtTcp = new JMenuItem( "TCP" );
+        JMenuItem mProtRmi = new JMenuItem( "RMI" );
+        JMenuItem mProtCorba = new JMenuItem( "CORBA" );
+        mProtocols.add( mProtUdp );
+        mProtocols.add( mProtTcp );
+        mProtocols.add( mProtRmi );
+        mProtocols.add( mProtCorba );
+
+        // Помощь
+        JMenu mHelp = new JMenu( "Помощь" );
+        JMenuItem mHelpAbout = new JMenuItem( "О программе..." );
+        mHelp.add( mHelpAbout );
+
+        JMenuBar bar = new JMenuBar();
+        setJMenuBar( bar );
+
+        bar.add( mFile );
+        bar.add( mProtocols );
+        bar.add( mHelp );
     }
 }
