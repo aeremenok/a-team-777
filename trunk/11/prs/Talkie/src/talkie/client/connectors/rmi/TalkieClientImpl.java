4dd0e977da0533e989c5c2880147f1a078929221
stas.sviridenko <stas.sviridenko@9273621a-9230-0410-b1c6-9ffd80c20a0c>
2008-12-12 11:52:30 +0000

    ПРС

--- trunk/11/prs/Talkie/src/talkie/client/connectors/rmi/TalkieClientImpl.java
+++ trunk/11/prs/Talkie/src/talkie/client/connectors/rmi/TalkieClientImpl.java
@@ -1,10 +1,12 @@
 package talkie.client.connectors.rmi;
 
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
 import talkie.client.connectors.RMIConnector;
 
 public class TalkieClientImpl
+    extends UnicastRemoteObject
     implements
         TalkieClient
 {
@@ -14,7 +16,9 @@ public class TalkieClientImpl
 
     public TalkieClientImpl(
         RMIConnector connector )
+        throws RemoteException
     {
+        super();
         this.connector = connector;
         this.login = connector.getLogin();
         this.pass = connector.getPass();
